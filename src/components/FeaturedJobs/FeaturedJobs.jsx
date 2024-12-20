b647777e2cb0ad015f0fd4234048d8c521877f31
shafayat666 <shafayatmahin5@gmail.com>
2024-12-20 10:26:19 +0600

    added local assets

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
+import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
+  const [jobs, setJobs] = useState([])
+
+  useEffect(() => {
+    fetch("jobs.json")
+      .then(res => res.json())
+      .then(data => setJobs(data))
+  }, [])
+
+  // console.log(jobs)
   return (
     <div className="text-center">
-      <div><h2 className="text-5xl">Featured Jobs</h2></div>      
-      <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+      <div>
+        <h2 className="text-5xl">Featured Jobs: {jobs.length}</h2>
+        <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+        <div>
+          {
+            jobs.map((job, index) => <Job key={index} job={job}></Job>)
+          }
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/Job/Job.jsx
@@ -0,0 +1,18 @@
+
+import PropTypes from 'prop-types';
+
+const Job = ({job}) => {
+  const {logo} = job;
+  console.log(logo);
+  return (
+    <div>
+      <img src={logo} alt="" />
+    </div>
+  );
+};
+
+Job.propTypes = {
+  job: PropTypes.object
+};
+
+export default Job;
